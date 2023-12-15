4ea8c400b712bdbbbe637be12fc9091b966f536c
mhmdelwan <elwan2771@gmail.com>
2023-12-16 00:39:20 +0200

    third commit

--- js/main.js
+++ js/main.js
@@ -43,3 +43,64 @@ for (let i = 0; i < link.length; i++) {
   });
 }
 // E N D   A C T I V E   L I N K S
+
+let btn1 = document.querySelector('.btn1');
+let btn2 = document.querySelector('.btn2');
+let sale = document.querySelector('.sale');
+let rent = document.querySelector('.rent');
+
+btn1.onclick = function(){
+  sale.style.display = 'block';
+  rent.style.display = 'none';
+}
+
+btn2.onclick = function(){
+  sale.style.display = 'none';
+  rent.style.display = 'block';
+}
+
+
+
+let btns = document.querySelectorAll(".btn");
+
+
+for (let b = 0; b < btns.length; b++) {
+  btns[b].addEventListener("click", function () {
+    for (let n = 0; n < btns.length; n++) {
+      btns[n].classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+}
+
+
+
+
+let header = document.querySelector('.header');
+
+window.onscroll = function(){
+  if(scrollY >=100){
+    header.style.background = "#f5f4f4";
+  } else
+  {
+    header.style.background = 'transparent'
+  }
+}
+
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
